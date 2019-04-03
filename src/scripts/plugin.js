c2f593f499af35f7dd1be827216439993f36ce31
Anton <samusenkoanton@yandex.ru>
2019-04-03 19:34:32 +0300

    add worked pie-chart and delete d3.js

--- src/scripts/plugin.js
+++ src/scripts/plugin.js
@@ -1,7 +1,5 @@
 import "./easy-piechart.js";
 import "./percentages.js";
 import "./ripple.js";
-import "./basic-donut.js"
-import "./test.js"
 import "../styles/main.scss";
 console.log('all files loaded');
